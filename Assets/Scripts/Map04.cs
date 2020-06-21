ca87a17a615cadd9022771416ce66a8099a0fc73
centerwing <2212310473@qq.com>
2020-06-21 22:51:02 +0800

    map manager&end point

--- Assets/Scripts/Map04.cs
+++ Assets/Scripts/Map04.cs
@@ -114,11 +114,12 @@ public class Map04 : IMapData {
     };
 	
 	Vector3 playerStart = new Vector3(3, 0, 3);
-	
-	public Vector3[] CubePos{ get{ return cubePos; } }
+    Vector3 gameEnd = new Vector3(0, 0, 0);
+
+    public Vector3[] CubePos{ get{ return cubePos; } }
 	public Vector3[] SlopePos{ get{ return slopePos; } }
 	public Quaternion[] SlopeRot{ get{ return slopeRot; } }
 	public RotatingObject[] RotObj{ get{ return rotObj; } }
 	public Vector3 PlayerStart{ get{ return playerStart; } }
-	
+    public Vector3 GameEnd { get { return gameEnd; } }
 }
